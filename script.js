d72e266f9281b2814f689be5c6f1816acb1efe83
kaykyveiga <kaykyveiga46@gmail.com>
2023-04-07 12:21:49 -0300

    calculadora análise combinatória

---
+++ script.js
@@ -0,0 +1,89 @@
+function calcularFatorial() {
+    let n1 = document.getElementById('n1').value;
+    n1 = parseFloat(n1);
+    let fatorial = 1;
+    for (let i = 1; i <= n1; i++) {
+        fatorial *= i;
+    }
+
+    const resultado = document.getElementById('resultado');
+    resultado.value = fatorial;
+
+    const mensagemFatorial = document.getElementById('mensagemFatorial');
+    mensagemFatorial.innerHTML = `A conta realizada foi ${n1}!`
+}
+
+function calcularDivisaoFatorial() {
+    let numerador = document.getElementById('numerador').value;
+    numerador = parseFloat(numerador);
+    let fatorialNumerador = 1;
+    for (let i = 1; i <= numerador; i++) {
+        fatorialNumerador *= i;
+    }
+
+    let denominador = document.getElementById('denominador').value;
+    denominador = parseFloat(denominador);
+    let fatorialDenominador = 1;
+    for (let i = 1; i <= denominador; i++) {
+        fatorialDenominador *= i;
+    }
+    let divisaoFatorial = fatorialNumerador / fatorialDenominador;
+
+    const mensagemDivisaoFatorial = document.getElementById('mensagemDivisaoFatorial');
+    mensagemDivisaoFatorial.innerHTML = `A conta realizada foi ${numerador}! / ${denominador}!`
+
+    const resultado = document.getElementById('resultadoDivisaoFatorial');
+    resultado.value = divisaoFatorial;
+}
+
+function calcularArranjo() {
+    let n = document.getElementById('n!').value;
+    n = parseFloat(n);
+    let nFatorial = 1;
+    for (let i = 1; i <= n; i++) {
+        nFatorial *= i;
+    }
+
+    let p = document.getElementById('p!').value;
+    p = parseFloat(p);
+    let pSubtracao = n - p;
+    let pFatorial = 1;
+    for (let i = 1; i <= pSubtracao; i++) {
+        pFatorial *= i;
+    }
+
+    let arranjo = nFatorial / pFatorial;
+
+    const mensagemArranjo = document.getElementById('mensagemArranjo');
+    mensagemArranjo.innerHTML = `A conta realizada foi ${n}! / (${n} - ${p})!`
+    const resultado = document.getElementById('resultadoArranjo');
+    resultado.value = arranjo;
+}
+
+function calcularCombinacao(){
+    let n = document.getElementById('N!').value;
+    n = parseFloat(n);
+    let nFatorial = 1;
+    for (let i = 1; i <= n; i++) {
+        nFatorial *= i;
+    }
+    let k = document.getElementById('k!').value;
+    k = parseFloat(k);
+    let kFatorial = 1;
+    for (let i = 1; i <= k; i++) {
+        kFatorial *= i;
+    }
+    let kSubtracao = n - k;
+    let kSubtracaoFatorial = 1;
+    for (let i = 1; i <= kSubtracao; i++) {
+        kSubtracaoFatorial *= i;
+    }
+
+    const mensagemCombinacao = document.getElementById('mensagemCombinacao');
+    mensagemCombinacao.innerText = `A conta realizada foi ${n}! / ${k}!(${n} - ${k})!`
+
+    let combinacao = nFatorial / (kFatorial * kSubtracaoFatorial);
+    const resultado = document.getElementById('resultadoCombinacao');
+    resultado.value = combinacao;
+
+}
